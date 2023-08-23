9cc29cedca88a2689ac89f59ad86c9f8901dac4d
DmalTzi <Audyomg@gmail.com>
2023-08-23 22:52:05 +0700

    init

---
+++ model/data.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+mongoose.connect(`mongodb+srv://hdrproject:50230@cluster0.ktm1unb.mongodb.net/?retryWrites=true&w=majority`,{dbName:"HDRProjecct"},{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify: false,
+}).catch(err=>console.log(err))
+
+mongoose.set('strictQuery', true);
+
+
+const dataSchema =mongoose.Schema({
+    userId:String,
+    TeacherUser:String,
+    TeacherName:String,
+    StudentNumber:Number,
+    StudentName:String,
+    Room:Number,
+    Number:Number,
+    Symptom:String,
+    Age:Number,
+    Weight:Number,
+    Cause:String,
+    Range:Number,
+    Temp:Number,
+    Date_poo:String,
+    Time_poo:String,
+    Poo_time:Number,
+    Detail:{
+        UserBy:String,
+        Serial:Number,
+        SendBy:String,
+        SendStatus:Boolean,
+        Time:String,
+        date:String
+    },
+})
+
+module.exports = mongoose.model('wtf', dataSchema)
---
+++ server.js
@@ -0,0 +1,28 @@
+const PORT =  3000
+const express = require("express")
+const app = express()
+const logger = require("logger")
+const cors = require("cors")
+const Data = require("./model/data")
+const data = require("./model/data")
+
+app.use(cors())
+
+app.get("/api/hello",(req,res)=>{
+    res.json({
+        data: "world"
+    })
+})
+
+app.get("/api/find",(req,res)=>{
+    Data.findOne().then((result)=>{
+        console.log(result)
+        res.send(result)
+    })
+    // console.log(data)
+    // res.send("ok")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Sever running port ${PORT}`)
+})
