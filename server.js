1ae2de14230c730ae4a654d7e1702152bb3b7f1d
DmalTzi <audyomg@gmail.com>
2023-08-24 12:52:07 +0700

    test api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const PORT =  12123
+const PORT =  3000
 const express = require("express")
 const app = express()
+const logger = require("logger")
 const cors = require("cors")
 const Data = require("./model/data")
 
@@ -12,13 +13,19 @@ app.get("/api/hello",(req,res)=>{
     })
 })
 
-app.get("/api/find",(req,res)=>{
-    Data.findOne().then((result)=>{
-        console.log(new Date())
-        res.send(result)
-    })
-    // console.log(data)
-    // res.send("ok")
+app.post("/api/find",(req,res)=>{
+    try {
+        const { key, value } = req.body;
+
+        // Fetch data from MongoDB to respond back
+        const fetchedData = Data.find().sort({ _id: -1 }).limit(1);
+
+        // Respond with fetched data
+        res.status(200).json(fetchedData);
+    } catch (error) {
+        console.error('Error handling data:', error);
+        res.status(500).json({ message: 'Error handling data' });
+    }
 })
 
 app.listen(PORT,()=>{
